a0eb98e650ab4600c706d1580f78da853bfab714
Daniel Romotsky <romotsky@gmail.com>
2019-12-06 10:54:44 -0800

    new updates

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,22 +1,31 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
 
+// empty($_POST['phone']) || removed!
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
+// $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = "romotsky@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@romotsky.me\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+$header = "From: dromotsk@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+$mail=mail($to, "Subject: $email_subject",$message );
+if($mail){
+echo "Thank you for using our mail form";
+}else{
+echo "Mail sending failed.";
+}
+
+// if(!mail($to, $subject, $body, $header))
+//   http_response_code(500);
 ?>
